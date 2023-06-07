0dcde3c667a6dbb52895071f901c99ec5a246ecc
Ng Wing Tat, David <i@ngwingt.at>
2023-06-07 14:49:01 +0800

    💄 Make header visible after visited

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,7 +2,7 @@
   <div>
     <header>
       <div style="display: flex; flex-wrap:wrap; background-color:#28646e; color: white; padding: 20px;">
-        <NuxtLink :to="{ name: 'index' }">
+        <NuxtLink :to="{ name: 'index' }" style="color: white; text-decoration: none;">
           <h1>LikeCoin NFT Book Press</h1>
         </NuxtLink>
         <div style="flex: 1" />
